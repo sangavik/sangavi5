a950cbcac6e913ead558b1571dbb28482b75510a
sangavik <31438501+sangavik@users.noreply.github.com>
2018-02-05 16:07:07 +0530

    string.C

---
+++ string.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char a[20];
+int n;
+printf("\n enter the string");
+scanf("%s",a);
+printf("\n enter the num of times");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("\n",a);
+}
+return 0;
+}
