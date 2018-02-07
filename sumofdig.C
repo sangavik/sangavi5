3e051dec44e5141928144df408c61ed7d7eaa319
sangavik <31438501+sangavik@users.noreply.github.com>
2018-02-07 15:51:14 +0530

    sumofdig.C

---
+++ sumofdig.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,sum=0;
+printf("\n enter the three values :");
+scanf(%d%d%d",&a,&b,&c);
+sum=a+b+c;
+printf("\n the sum is:");
+scanf("%d",&sum);
+return 0;
+}
