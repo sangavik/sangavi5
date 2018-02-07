ec8bc1a6bce24bd9c6008cc8434ea215f280c800
sangavik <31438501+sangavik@users.noreply.github.com>
2018-02-07 16:06:07 +0530

    FIBONACCI.C

---
+++ FIBONACCI.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int fib1=0,fib2=1,fib3,limit,count++;
+printf("\n enter the limit of fibonacci series");
+scanf("%d\n",&limit);
+printf("\n fibonacci series is....");
+printf("\n",fib1);
+printf("\n",fib2);
+count=2;
+while(count<limit)
+{
+fib3=fin1+fib2;
+count++;
+printf("\n fib3);
+fib1=fib2;
+fib2=fib3;
+}
+return 0;
+}
