091f921f5a663d38327b4b2b982681c1ef59333a
sangavik <31438501+sangavik@users.noreply.github.com>
2018-02-07 15:46:37 +0530

    swap.C

---
+++ swap.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("\n enter two nos:");
+scanf("%d%d",&a,&b);
+printf("\n before swapping values are:"):
+swap(a,b)
+{
+a=a+b;
+b=a-b;
+a=a+b;
+printf("\n after swapping values are:");
+}
+return 0;
+}
